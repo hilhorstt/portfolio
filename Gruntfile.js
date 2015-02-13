93040d13659deea7b96929eedfe3fc37ef4e122d
hilhorstt <tim.hilhorst@gmail.com>
2015-02-13 23:08:09 +0100

    git settings

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'css/styling.css' : 'sass/styling.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  
+  // Default task(s).
+  grunt.registerTask('watch', ['watch']);
+  grunt.registerTask('default', ['sass']);
+
+};
