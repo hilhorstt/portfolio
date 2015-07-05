7b074319e8abac8b98931f584b76f3f798aff3e6
hilhorstt <tim.hilhorst@gmail.com>
2015-07-05 20:28:15 +0200

    Portfolio delivery for production

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -7,27 +7,12 @@ app.controller('portfolioContact', function($scope) {
 
 app.controller('portfolioMenu', function($scope) {
 	'use strict';
-    $scope.menuTopToSide = function() {
-    	var header = document.querySelector('.menu');
-    	var headerHeight = header.offsetHeight;
-        window.addEventListener('scroll', function(e){
-    	    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-    	    moveToLeftOn = 250;
-    	    if (distanceY < moveToLeftOn) {
-				header.style.height = (- distanceY) + headerHeight + "px";
-    	    }
-    	    console.log(distanceY + headerHeight);
-    	    console.log(distanceY);
 
-    	    if (distanceY > moveToLeftOn) {
-    	    	header.style.height = "";
-    	        header.classList.add('smaller');
-    	    } 
-    	    else {
-    	        if (header.classList.contains('smaller')) {
-    	            header.classList.remove('smaller');
-    	        }
-    	    }
-    	});
+    $scope.clickOpen = function () {
+        // body...
+        var menuSide = document.querySelector('.menu');
+        window.addEventListener('click', function(e) {
+
+        });
     }();
 });
