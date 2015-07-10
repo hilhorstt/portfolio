3e151df59de24afa7ffdfd19776ea2ca6bb4e7ef
hilhorstt <tim.hilhorst@gmail.com>
2015-07-10 16:00:23 +0200

    new styling

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -15,4 +15,19 @@ app.controller('portfolioMenu', function($scope) {
 
         });
     }();
+
+    var sectionBg = document.getElementsByClassName('introduction')[0];
+    var logo = document.getElementsByClassName('th-logo')[0];
+    var testimonialsInView = document.getElementsByClassName("testimonials")[0].getBoundingClientRect();
+    console.log(testimonialsInView);
+
+    window.addEventListener('scroll', function() {
+        sectionBg.style.backgroundPosition = "50% "+ pageYOffset / 2 +"px";
+        logo.style.webkitFilter = "blur("+ (pageYOffset/50) +"px)";
+        if(testimonialsInView === pageYOffset) {
+            console.log("bingo");
+            testimonialsInView.classList.add('animate');
+        }
+    });
+
 });
